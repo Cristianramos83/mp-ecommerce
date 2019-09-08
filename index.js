60e5c3f8d4a61f4ebc6acc370eff20f1018ee87b
Horacio López <hdlopez@gmail.com>
2019-09-08 13:39:20 -0300

    Added some fixes and documentation

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const port = process.env.PORT || 3000
 
 app.use(express.static('public'))
 
+app.post('/pay', function(req, res) {
+  // show congrats.html page example
+  res.sendFile(__dirname + "/public/congrats.html");
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
