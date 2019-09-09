3aca767589129637cc615f7f2b3f8d05a7d81525
Horacio López <hdlopez@gmail.com>
2019-09-09 16:36:21 -0300

    Added body parser dependency to handle request bodies on post http method

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000
+
+// Attach the body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('public'))
 
 app.post('/pay', function(req, res) {
+  console.log(req.body)
+  
   // show congrats.html page example
   res.sendFile(__dirname + "/public/congrats.html");
 });
